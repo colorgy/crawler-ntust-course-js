1c9985a9a260e12d1a88bfee145f5c3ae0edc2fd
Neson <neson@dex.tw>
2015-09-28 07:56:47 +0800

    build a console

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 import test from "tape"
-import crawlerNtustCourse from "../"
+import crawlerNTUSTCourse from "../"
 
 test("crawlerNtustCourse", (t) => {
   t.plan(1)
-  t.equal(true, crawlerNtustCourse(), "return true")
+  t.equal(true, crawlerNTUSTCourse(), "return true")
 })
